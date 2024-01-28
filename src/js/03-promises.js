32ddec85ab35327e8b83a56e4ea2464b90b2756f
Andree-Ioana <ioana.vinereanu@stud.etti.upb.ro>
2024-01-28 13:42:07 +0200

    promise generator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+const delayInput = document.querySelector('[name="delay"]');
+const stepInput = document.querySelector('[name="step"]');
+const amountInput = document.querySelector('[name="amount"]');
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
